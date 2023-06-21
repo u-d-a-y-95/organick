8a22e3299a2bdbe4755c015becf82d7ba2acb100
UDay <uday.dev.bd@gmail.com>
2023-06-21 10:52:11 +0600

    design header layout

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+const navs = [
+  {
+    label: "home",
+    url: "/",
+  },
+  {
+    label: "Shop",
+    url: "/",
+  },
+  {
+    label: "Pages",
+    url: "/",
+  },
+  {
+    label: "Projects",
+    url: "/",
+  },
+  {
+    label: "News",
+    url: "/",
+  },
+  {
+    label: "About",
+    url: "/",
+  },
+];
+
+export default function Navbar() {
+  return (
+    <div className="flex text-primary capitalize justify-center gap-10 font-semibold">
+      {navs.map((nav, index) => (
+        <Link key={index} href={nav.url} className="hover:text-secondary">
+          {nav.label}
+        </Link>
+      ))}
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#274C5B",
+        "primary-dark": "#525C60",
+        secondary: "#7EB693",
+        "secondary-light": "#C9DECF",
+        accent: "#EFD372",
+        "secondary-accent": "#D4D4D4",
+        "secondary-accent-light": "#F9F8F8",
       },
     },
   },
   plugins: [],
-}
+};
