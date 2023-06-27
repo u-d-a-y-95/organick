715c3540ab082dad74f8ce1405e0c3bdd8330f63
UDay <uday.dev.bd@gmail.com>
2023-06-27 14:55:54 +0600

    add testimonial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/button";
 import OfferBanner from "@/components/offer-banner";
 import ProductCard from "@/components/product-card";
+import TestimonialRoundCard from "@/components/testimonial-round-card";
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/solid";
 import { Yellowtail } from "next/font/google";
 import Image from "next/image";
@@ -68,7 +69,7 @@ export default function Home() {
           backgroundPosition: "center",
           backgroundSize: "cover",
         }}
-        className="h-[500px] flex justify-center items-center"
+        className="h-[400px] sm:h-[650px] flex justify-center items-center"
       >
         <div className="w-[400px]">
           <h3 className={`${yellowtail.className} text-secondary text-xlx`}>
@@ -81,12 +82,12 @@ export default function Home() {
             <Button label="Explore Now" className="" />
           </div>
         </div>
-        <div className="w-[600px] h-full relative">
+        <div className="hidden md:block w-[600px] h-full relative">
           <Image fill={true} src="/img/home-banner-1.png" alt="banner" />
         </div>
       </div>
       {/* offer banner */}
-      <div className="py-10 flex flex-col md:flex-row justify-center items-center gap-10">
+      <div className="my-16 py-10 flex flex-col md:flex-row justify-center items-center gap-10">
         <OfferBanner
           src="/img/offer-banner-1.png"
           title="Get Garden Fresh Fruits"
@@ -101,9 +102,65 @@ on Vegetables"
         />
       </div>
 
+      <div
+        style={{
+          background: "#F1EFF0",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+        className="h-[600px] flex justify-center items-center"
+      >
+        <div className="hidden md:block w-[600px] h-full relative">
+          <Image fill={true} src="/img/home-banner-1.png" alt="banner" />
+        </div>
+        <div className="w-[400px]">
+          <h3 className={`${yellowtail.className} text-secondary text-xlx`}>
+            About Us
+          </h3>
+          <h2 className="text-4xl font-extrabold mt-2">
+            We Believe in Working Accredited Farmers
+          </h2>
+          <p className="text-primary-dark text-sm font-thin my-3">
+            Simply dummy text of the printing and typesetting industry. Lorem
+            had ceased to been the industry's standard dummy text ever since the
+            1500s, when an unknown printer took a galley.
+          </p>
+          <div className="my-5 flex flex-col gap-4">
+            <div className="flex gap-3 items-center">
+              <div className="relative h-16 w-24">
+                <Image src="/img/card-1.png" fill={true} alt="card" />
+              </div>
+              <div className="flex justify-between flex-col">
+                <h5 className="font-semibold">Organic Foods Only</h5>
+                <p className="text-sm font-thin">
+                  Simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-3 items-center">
+              <div className="relative h-16 w-24">
+                <Image src="/img/card-2.png" fill={true} alt="card" />
+              </div>
+              <div className="flex justify-between flex-col">
+                <h5 className="font-semibold">Quality Standards</h5>
+                <p className="text-sm font-thin">
+                  Simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-8">
+            <Button label="Shop Now" className="bg-primary text-white" />
+          </div>
+        </div>
+      </div>
+
       {/* products */}
-      <div className="text-center">
-        <div className="p-5 sm:0 container mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-4 sm:gap-10">
+      <div className="text-center mt-10">
+        <h3 className="text-4xl font-bold my-5">Our Products</h3>
+        <div className="p-5 sm:0 container mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-4 sm:gap-7">
           {products.map((product, index) => (
             <ProductCard key={index} {...product} />
           ))}
@@ -112,6 +169,50 @@ on Vegetables"
           <button className="bg-primary text-white rounded-lg p-4 px-8 text-sm font-semibold">
             Load More
           </button>
+        </div>
+      </div>
+
+      {/* testimonial */}
+      <div
+        style={{
+          background: "#F1EFF0 url('/img/testimonial-1.png')",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+        className="flex flex-col justify-center items-center"
+      >
+        <div className="flex flex-col items-center mt-20">
+          <h3 className={`${yellowtail.className} text-secondary text-xlx`}>
+            Testimonial
+          </h3>
+          <h2 className="text-4xl font-extrabold mt-2 text-center">
+            What Our Customer Saying?
+          </h2>
+          <div className="my-10">
+            <div className="w-96 text-center">
+              <div className="flex justify-center">
+                <div className="w-20 h-20 bg-gray-200 rounded-full"></div>
+              </div>
+
+              <p className="text-sm font-thin my-2">
+                Simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been.
+              </p>
+
+              <div className="leading-tight mt-3">
+                <h5 className="">Sara Tylor</h5>
+                <span className="text-[12px] font-thin">Consumer</span>
+              </div>
+            </div>
+          </div>
+          <div className="border w-full mb-10"></div>
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-4 gap-10 mb-20">
+          <TestimonialRoundCard title="100%" subtitle="Organic" />
+          <TestimonialRoundCard title="285" subtitle="Active product" />
+          <TestimonialRoundCard title="350+" subtitle="Organic Orchads" />
+          <TestimonialRoundCard title="25+" subtitle="Years of Farming" />
         </div>
       </div>
     </>
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,11 +5,13 @@ interface IButtonProps {
   className: string;
 }
 
-export default function Button({ label }: IButtonProps) {
+export default function Button({ label, className }: IButtonProps) {
   return (
-    <button className="bg-accent rounded-lg p-4 px-8 flex items-center gap-2 text-sm font-semibold group/btn">
+    <button
+      className={`bg-accent rounded-lg p-4 px-8 flex items-center gap-2 text-sm font-semibold ${className}`}
+    >
       {label}
-      <ArrowRightIcon className="w-4 h-4 bg-primary text-white rounded-full p-1  animate-infinite ease-in-out delay-150  group-hover/btn:translate-x-6" />
+      <ArrowRightIcon className="w-4 h-4 bg-primary text-white rounded-full p-1" />
     </button>
   );
 }
