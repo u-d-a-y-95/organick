dc43f6379af3eda20df422789d5ced524b8556ad
UDay <uday.dev.bd@gmail.com>
2023-06-27 13:31:10 +0600

    add products section in the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,62 @@
 import Button from "@/components/button";
-import OfferBanner from "@/components/offerBanner";
+import OfferBanner from "@/components/offer-banner";
+import ProductCard from "@/components/product-card";
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/solid";
+import { Yellowtail } from "next/font/google";
 import Image from "next/image";
+
+const yellowtail = Yellowtail({ weight: "400", subsets: ["latin"] });
+
+const products = [
+  {
+    name: "Calabrese Broccoli",
+    categoryName: "Vegetables",
+    url: "/img/category-1.png",
+    price: 20,
+  },
+  {
+    name: "Calabrese Broccoli",
+    categoryName: "Vegetables",
+    url: "/img/category-1.png",
+    price: 20,
+  },
+  {
+    name: "Calabrese Broccoli",
+    categoryName: "Vegetables",
+    url: "/img/category-1.png",
+    price: 20,
+  },
+  {
+    name: "Calabrese Broccoli",
+    categoryName: "Vegetables",
+    url: "/img/category-1.png",
+    price: 20,
+  },
+  {
+    name: "Calabrese Broccoli",
+    categoryName: "Vegetables",
+    url: "/img/category-1.png",
+    price: 20,
+  },
+  {
+    name: "Calabrese Broccoli",
+    categoryName: "Vegetables",
+    url: "/img/category-1.png",
+    price: 20,
+  },
+  {
+    name: "Calabrese Broccoli",
+    categoryName: "Vegetables",
+    url: "/img/category-1.png",
+    price: 20,
+  },
+  {
+    name: "Calabrese Broccoli",
+    categoryName: "Vegetables",
+    url: "/img/category-1.png",
+    price: 20,
+  },
+];
 
 export default function Home() {
   return (
@@ -16,12 +71,14 @@ export default function Home() {
         className="h-[500px] flex justify-center items-center"
       >
         <div className="w-[400px]">
-          <h3 className="text-secondary italic">100% Natural Food</h3>
+          <h3 className={`${yellowtail.className} text-secondary text-xlx`}>
+            100% Natural Food
+          </h3>
           <h2 className="text-4xl font-extrabold mt-2">
             Choose the best healthier way of life
           </h2>
           <div className="mt-8">
-            <Button />
+            <Button label="Explore Now" className="" />
           </div>
         </div>
         <div className="w-[600px] h-full relative">
@@ -29,7 +86,7 @@ export default function Home() {
         </div>
       </div>
       {/* offer banner */}
-      <div className=" h-[350px] flex justify-center items-center gap-10">
+      <div className="py-10 flex flex-col md:flex-row justify-center items-center gap-10">
         <OfferBanner
           src="/img/offer-banner-1.png"
           title="Get Garden Fresh Fruits"
@@ -42,6 +99,20 @@ export default function Home() {
 on Vegetables"
           subTitle="Offer!!"
         />
+      </div>
+
+      {/* products */}
+      <div className="text-center">
+        <div className="p-5 sm:0 container mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-4 sm:gap-10">
+          {products.map((product, index) => (
+            <ProductCard key={index} {...product} />
+          ))}
+        </div>
+        <div className="my-16">
+          <button className="bg-primary text-white rounded-lg p-4 px-8 text-sm font-semibold">
+            Load More
+          </button>
+        </div>
       </div>
     </>
   );
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,9 +1,14 @@
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
-export default function Button() {
+interface IButtonProps {
+  label: string;
+  className: string;
+}
+
+export default function Button({ label }: IButtonProps) {
   return (
     <button className="bg-accent rounded-lg p-4 px-8 flex items-center gap-2 text-sm font-semibold group/btn">
-      Explore Now
+      {label}
       <ArrowRightIcon className="w-4 h-4 bg-primary text-white rounded-full p-1  animate-infinite ease-in-out delay-150  group-hover/btn:translate-x-6" />
     </button>
   );
